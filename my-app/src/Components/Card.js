3d784f1c4731bb1426145c9c94c27a0bef219a77
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-10-05 22:49:33 +0300

    passing down data to elements

--- my-app/src/Components/Card.js
+++ my-app/src/Components/Card.js
@@ -1,18 +1,21 @@
 import React from "react";
 
-const Card = () => {
+const Card = (movie) => {
+    console.log(movie.info);
+    let img_path = "https://image.tmdb.org/t/p/w500";
   return (
     <>
       <div>
         <div className="movie">
-        <img src="Images/poster.jpg" className="poster"></img>
+        <img src= {img_path + movie.info.poster_path} className="poster"></img>
         <div className="movie-details">
           <div className="box">
-            <h4 className="title">Movie Title:</h4>
-            <p className="rating"> 9.7</p>
+            <h4 className="title">{movie.info.title}</h4>
+            <p className="rating">{movie.info.vote_average}</p>
             <div className="overview">
               <h2>overview</h2>
-              Lorem ipsum dolor sit amet
+              {movie.info.overview}
+              
               </div>
             </div>
           </div>
