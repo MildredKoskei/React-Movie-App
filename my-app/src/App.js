1cca1220865bfc2fea095fda1589022e1f7910a2
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-10-06 15:30:09 +0300

    adding inline styling to h1 tag

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Main from './Components/Main'
-
+import "./Components/style.css"
 
 function App() {
   return (
     <>
+    <h1 style = {{color: "white"}}>Discover AND WATCH YOUR AMAZING MOVIES!</h1>
     <Main />
     </>
   )
