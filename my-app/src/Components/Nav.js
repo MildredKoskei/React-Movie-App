58cb2853f34c3b5558f5d1a708cb543ab0edc8b9
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-10-06 22:17:20 +0300

    adding routing to the nav bar

--- my-app/src/Components/Nav.js
+++ my-app/src/Components/Nav.js
@@ -4,7 +4,32 @@ import {link} from "react-router-dom"
 function Nav() {
   return (
     <>
-   
+    <div className="navbar">
+      <NavLink to="/" exact>
+        Home
+      </NavLink>
+
+      <NavLink to="/" exact>
+        Popular
+      </NavLink>
+
+      <NavLink to="/" exact>
+        Theatre
+      </NavLink>
+
+      <NavLink to="/" exact>
+        Kids
+      </NavLink>
+
+      <NavLink to="/" exact>
+        Drama
+      </NavLink>
+
+      <NavLink to="/" exact>
+        Comedy
+      </NavLink>
+
+    </div>
     </>
   )
 }
